7d0570e7a941581b362d42fb2cfde665310cbead
Timothy Walker <timothy.l.walker1@gmail.com>
2019-04-25 17:55:35 -0700

    Moved data files into data folder; Modified scraper to save to data folder

--- webscraper.py
+++ webscraper.py
@@ -11,11 +11,13 @@
 #
 
 # library imports
+import os, sys
 from urllib.request import Request, urlopen
 import bs4 as bs
 import pandas as pd
 
 # Set year and genres to scrape, ending year not inclusive
+dataPath = './data/'
 yearStart = 2000
 yearEnd = 2011
 genreList = ['r-b-hip-hop-songs', 'rap-song', 'country-songs',
@@ -26,8 +28,11 @@ for genre in genreList:
 
     print('Grabbing genre: ' + genre)
 
+    if not os.path.exists('data'):
+            os.mkdir('data') 
+
     # Write header
-    file = open(genre + ".csv", "w")
+    file = open('data/' + genre + ".csv", "w")
     file.write('Year,Title,Artist\n')
     file.close()
 
@@ -46,7 +51,8 @@ for genre in genreList:
         df.columns = ['Year', 'Title', 'Artist']
         df.drop_duplicates(subset='Title', inplace=True)
         df['Year'] = year
+     
 
         # Print to .csv
-        df.to_csv(path_or_buf=genre + '.csv',
+        df.to_csv(path_or_buf='./data/' + genre + '.csv',
                   header=False, mode='a', index=False)
